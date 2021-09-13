f27e91880ca1158f8fe8a9ab49b02d04e9c791c1
Philip Herron <herron.philip@googlemail.com>
2021-09-13 11:17:37 +0100

    add tweak to pr org mode formattor helper

--- util/pr-org.py
+++ util/pr-org.py
@@ -2,14 +2,26 @@
 
 import sys
 
-def generate_org_line(prnum):
-    return "[[https://github.com/Rust-GCC/gccrs/pull/{0}][PR{0}]]".format(prnum)
+
+def generate_url(prnum):
+    return "https://github.com/Rust-GCC/gccrs/pull/{0}".format(prnum)
+
+
+def generate_desc(prurl):
+    return "FIXME"
+
+
+def generate_org_line(desc, prurl, prnum):
+    return "- {0} [[{1}][PR{2}]]".format(desc, prurl, prnum)
 
 
 def main():
     for prnum in sys.argv[1:]:
-        line = generate_org_line(prnum)
+        prurl = generate_url(prnum)
+        desc = generate_desc(prurl)
+        line = generate_org_line(desc, prurl, prnum)
         print(line)
 
+        
 if __name__ == "__main__":
     main()
