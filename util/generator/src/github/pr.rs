225d7487758d1de7a5ca7b7de5c689a2c3b52eef
Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>
2024-08-05 11:27:49 +0200

    WIP thingy
    Signed-off-by: Pierre-Emmanuel Patry <pierre-emmanuel.patry@embecosm.com>

--- util/generator/src/github/pr.rs
+++ util/generator/src/github/pr.rs
@@ -3,11 +3,12 @@ use crate::{
     error::Error,
     naming::{ORGANISATION, REPOSITORY},
 };
-use chrono::NaiveDate;
+use chrono::{offset::Utc, DateTime, NaiveDate};
 use octocrab::{models::pulls::PullRequest, params::State, Octocrab};
 
 use std::fmt::{Display, Formatter, Result as FmtResult};
 
+#[derive(Clone)]
 pub struct Pr {
     number: u64,
     url: String,
@@ -66,4 +67,20 @@ pub async fn fetch_merged(
                 .unwrap_or(true)
         })
         .collect())
+}
+
+pub async fn find_oldest_pr_merge_commit(gh: &Octocrab, prs: &[Pr]) -> Option<String> {
+    let mut oldest: Option<(DateTime<Utc>, Option<String>)> = None;
+    for pr in prs {
+        let complete_pr = match gh.pulls(ORGANISATION, REPOSITORY).get(pr.number).await {
+            Ok(x) if x.merged_at.is_some() => x,
+            _ => continue,
+        };
+        oldest = match oldest {
+            Some(ref old_date) if old_date.0 < complete_pr.merged_at.unwrap() => oldest,
+            // None or older
+            _ => Some((complete_pr.merged_at.unwrap(), complete_pr.merge_commit_sha)),
+        }
+    }
+    oldest.map(|(_, pr)| pr).flatten()
 }
